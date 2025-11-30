11ebbd3013ca586f4ed6e5dbf81e5e011b241b36
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-11-30 18:40:09 +0300

    tests

--- handlers/drinks.py
+++ handlers/drinks.py
@@ -7,8 +7,8 @@ from interfaces.database import Database
 
 class DrinksHandler(Handler):
     def __init__(self, telegram: TelegramClient, db: Database):
-        self.telegram = telegram
-        self.db = db
+        self.telegram: TelegramClient = telegram
+        self.db: Database = db
 
     def check_update(self, update: dict) -> bool:
         return "callback_query" in update and update["callback_query"][
